33fe4fc5b85c4fb3c3617426382a611d89a9c90f
Alireza Mohammadian <alirezamohammadian@Alirezas-Mac-mini.local>
2023-09-15 13:20:29 -0700

    search user

--- lib/screen/search_Screen.dart
+++ lib/screen/search_Screen.dart
@@ -1,14 +1,32 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:fluttertest1/screen/post_screan.dart';
 import 'package:fluttertest1/util/image_save.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   SearchScreen({Key? key}) : super(key: key);
 
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  bool show = true;
+  final search = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    search.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
@@ -18,7 +36,8 @@ class SearchScreen extends StatelessWidget {
           SliverToBoxAdapter(
             child: _getSearchBox(),
           ),
-          StreamBuilder<QuerySnapshot>(
+          if (show)
+            StreamBuilder<QuerySnapshot>(
               stream: _firestore.collection("posts").snapshots(),
               builder: (context, snapshot) {
                 if (!snapshot.hasData) {
@@ -68,7 +87,44 @@ class SearchScreen extends StatelessWidget {
                     ),
                   ),
                 );
-              })
+              },
+            ),
+          if (!show)
+            StreamBuilder(
+              stream: _firestore
+                  .collection('users')
+                  .where('username', isGreaterThanOrEqualTo: search.text)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const SliverToBoxAdapter(
+                      child: Center(child: CircularProgressIndicator()));
+                }
+                return SliverPadding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 4.w, vertical: 15.h),
+                  sliver: SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                        final snap = snapshot.data!.docs[index];
+                        return Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 23.r,
+                              backgroundImage:
+                                  NetworkImage(snap['profileImage']),
+                            ),
+                            SizedBox(width: 15.w),
+                            Text(snap['username']),
+                          ],
+                        );
+                      },
+                      childCount: snapshot.data!.docs.length,
+                    ),
+                  ),
+                );
+              },
+            )
         ],
       )),
     );
@@ -80,7 +136,7 @@ class SearchScreen extends StatelessWidget {
       margin: EdgeInsets.only(left: 4.w, right: 4.w, top: 12.h),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.all(Radius.circular(13.r)),
-        color: Colors.grey[350],
+        color: Colors.grey[500],
       ),
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 10.w),
@@ -93,9 +149,19 @@ class SearchScreen extends StatelessWidget {
             SizedBox(
               width: 15.w,
             ),
-            const Expanded(
+            Expanded(
               child: TextField(
-                decoration: InputDecoration(
+                controller: search,
+                onChanged: (value) {
+                  setState(() {
+                    if (value.length > 0) {
+                      show = false;
+                    } else {
+                      show = true;
+                    }
+                  });
+                },
+                decoration: const InputDecoration(
                   hintText: 'Search User',
                   hintStyle: TextStyle(color: Colors.white),
                   enabledBorder: InputBorder.none,
